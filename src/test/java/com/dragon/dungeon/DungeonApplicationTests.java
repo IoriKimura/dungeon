ce8f0e54a1d88e5f5637961f73c1fcb477ab6d03
IoriKimura <sergeykonstwork@gmail.com>
2024-04-15 00:25:52 +0300

    Init commit

---
+++ src/test/java/com/dragon/dungeon/DungeonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dragon.dungeon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DungeonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
